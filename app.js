c32b22ccf7b8d8119175631384deafc534166711
Nick Foden <nickfoden@gmail.com>
2017-03-07 21:56:06 -0500

    Fixed missing semicolons

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ var addItem = function(state, text) {
 var renderList = function(state, element){
 	var itemHTML = "";
 	for (i = 0; i < state.items.length; i++){
-		itemHTML += "<li>"
+		itemHTML += "<li>";
 		itemHTML += '<span class="shopping-item'; 
 		if (state.items[i].checked){
-			itemHTML += ' shopping-item-checked'
+			itemHTML += ' shopping-item-checked';
 		};
 		itemHTML += '">' + state.items[i].text + '</span>';
 		itemHTML += '<div class="shopping-item-controls">';
@@ -37,6 +37,7 @@ $('#js-shopping-list-form').submit(function(event) {
     addItem(state, $('#shopping-list-entry').val());
  	renderList(state, $('.shopping-list'));
 });
+
 
 
 
